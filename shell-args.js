cec799bc4bd4b8c1a3a17cf188699034220088b0
Stefan von der Krone <stefanvonderkrone@gmx.de>
2014-10-31 15:05:08 +0100

    minor changes

--- shell-args.js
+++ shell-args.js
@@ -16,6 +16,7 @@ module.exports = {
             "markdown/acknowledgements.md",
             "markdown/toc.md",
             "markdown/introduction/intro1.md",
+            "markdown/introduction/intro2.md",
             "markdown/chapters/chapter1_Termination_Competition.md",
             "markdown/chapters/chapter2_StarExec.md",
             "markdown/chapters/chapter3_StarExecPresenter.md",
